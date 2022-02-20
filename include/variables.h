d53415296b4196607105a70d7f59bbf4947da803
Sera91 <serafina.digioia@inaf.it>
2022-02-20 12:26:15 +0100

    refactoring ljmd libraries

---
+++ include/variables.h
@@ -0,0 +1,14 @@
+
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+
+/* generic file- or pathname buffer length */
+#define BLEN 200
+
+
+/* a few physical constants */
+const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
+const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
+
+#endif
