c1cd9895a35f88a5fefbfcf9691334486c22f418
Debarshi Banerjee <45908114+debarshibanerjee@users.noreply.github.com>
2022-02-20 13:22:27 +0100

    Some small modifications to ensure correct serial output, added clang-format template

--- include/prototypes.h
+++ include/prototypes.h
@@ -1,34 +1,29 @@
 #ifndef PROTOTYPES_H
 #define PROTOTYPES_H
 
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <math.h>
-#include <sys/time.h>
 #include "structures.h"
 #include "variables.h"
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
 
-//I/O function
-int get_a_line(FILE *fp, char *buf);
-void output(mdsys_t *sys, FILE *erg, FILE *traj);
+// I/O function
+static int get_a_line(FILE* fp, char* buf);
+static void output(mdsys_t* sys, FILE* erg, FILE* traj);
 
-//utilities
+// utilities
 double wallclock();
-void azzero(double *d, const int n);
+void azzero(double* d, const int n);
 double pbc(double x, const double boxby2);
 
-//physical functions
-
-void force(mdsys_t *sys);
-void ekin(mdsys_t *sys);
-void verlet_vel_update(mdsys_t *sys);
-void verlet_vel_propagation(mdsys_t *sys);
-static void velverlet(mdsys_t *sys);
-
-
-
+// physical functions
+void force(mdsys_t* sys);
+void ekin(mdsys_t* sys);
+void verlet_vel_propagation(mdsys_t* sys);
+void verlet_vel_update(mdsys_t* sys);
+static void velverlet(mdsys_t* sys);
 
 #endif
-
--- src/verlet.c
+++ src/verlet.c
@@ -1,50 +1,37 @@
-#include "structures.h"
 #include "prototypes.h"
 
+/* velocity verlet propagation step*/
+void verlet_vel_propagation(mdsys_t* sys) {
+	int i;
 
-
-
-/* velocity verlet */
-void verlet_vel_propagation(mdsys_t *sys) {
-    int i;
-
-    /* first part: propagate velocities by half and positions by full step */
-    for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
-        sys->rx[i] += sys->dt*sys->vx[i];
-        sys->ry[i] += sys->dt*sys->vy[i];
-        sys->rz[i] += sys->dt*sys->vz[i];
-    }
-
+	/* first part: propagate velocities by half and positions by full step */
+	for (i = 0; i < sys->natoms; ++i) {
+		sys->vx[i] += 0.5 * sys->dt / mvsq2e * sys->fx[i] / sys->mass;
+		sys->vy[i] += 0.5 * sys->dt / mvsq2e * sys->fy[i] / sys->mass;
+		sys->vz[i] += 0.5 * sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+		sys->rx[i] += sys->dt * sys->vx[i];
+		sys->ry[i] += sys->dt * sys->vy[i];
+		sys->rz[i] += sys->dt * sys->vz[i];
+	}
 }
 
-
-
-void verlet_vel_update(mdsys_t *sys) {
-
-    /* compute forces and potential energy */
-    force(sys);
-
-    /* second part: propagate velocities by another half step */
-    for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
-    }
+/* velocity verlet update step*/
+void verlet_vel_update(mdsys_t* sys) {
+	/* second part: propagate velocities by another half step */
+	for (i = 0; i < sys->natoms; ++i) {
+		sys->vx[i] += 0.5 * sys->dt / mvsq2e * sys->fx[i] / sys->mass;
+		sys->vy[i] += 0.5 * sys->dt / mvsq2e * sys->fy[i] / sys->mass;
+		sys->vz[i] += 0.5 * sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+	}
 }
 
+static void velverlet(mdsys_t* sys) {
+	/* initial velocity propagation */
+	verlet_vel_propagation(sys);
 
-static void velverlet(mdsys_t *sys) {
+	/* compute forces and potential energy */
+	force(sys);
 
-    verlet_vel_propagation(sys);
-    /* compute forces and potential energy */
-    force(sys);
-
-    /*callinf the fucntion that computes velocities again*/
-
-    verlet_vel_update(sys);
+	/* update the velocities */
+	verlet_vel_update(sys);
 }
-
-
