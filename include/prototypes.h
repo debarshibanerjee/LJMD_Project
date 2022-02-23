83103c56e7f1e661a90a25bebe4aa1a695746b46
Sera91 <serafina.digioia@inaf.it>
2022-02-23 11:55:06 +0100

    modified prototypes.h

--- include/prototypes.h
+++ include/prototypes.h
@@ -10,19 +10,10 @@
 #include <string.h>
 #include <sys/time.h>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
 #ifdef _OPENMP
-#	include <omp.h>
+#include <omp.h>
 #endif
->>>>>>> 6aca630b002cca78d5634cadbcd83271fba000e5
-=======
-#ifdef _OPENMP
-#	include <omp.h>
-#endif
->>>>>>> 6aca630b002cca78d5634cadbcd83271fba000e5
 
 #ifdef __cplusplus
 extern "C"
@@ -32,9 +23,6 @@ extern "C"
 //I/O function
 extern int get_a_line(FILE *fp, char *buf);
 extern void output(mdsys_t *sys, FILE *erg, FILE *traj);
-<<<<<<< HEAD
-
-<<<<<<< HEAD
 
 
 //physical functions
@@ -47,39 +35,6 @@ static void velverlet(mdsys_t *sys);
 extern void verlet_1(mdsys_t *sys);
 extern void verlet_2(mdsys_t *sys);
 
-=======
-//utilities
-extern double wallclock();
-extern void azzero(double *d, const int n);
-extern double pbc(double x, const double boxby2);
-
-//physical functions
-
-extern void force(mdsys_t *sys);
-extern void ekin(mdsys_t *sys);
-extern void verlet_vel_update(mdsys_t *sys);
-extern void verlet_vel_propagation(mdsys_t *sys);
-extern void verlet_1(mdsys_t *sys);
-extern void verlet_2(mdsys_t *sys);
-=======
-
-//utilities
-extern double wallclock();
-extern void azzero(double *d, const int n);
-extern double pbc(double x, const double boxby2);
-
-//physical functions
-
-extern void force(mdsys_t *sys);
-extern void ekin(mdsys_t *sys);
-extern void verlet_vel_update(mdsys_t *sys);
-extern void verlet_vel_propagation(mdsys_t *sys);
-extern void verlet_1(mdsys_t *sys);
-extern void verlet_2(mdsys_t *sys);
->>>>>>> 6aca630b002cca78d5634cadbcd83271fba000e5
-extern void velverlet(mdsys_t *sys);
-extern void doublesleep(double t);
->>>>>>> 6aca630b002cca78d5634cadbcd83271fba000e5
 
 #ifdef __cplusplus
 }
