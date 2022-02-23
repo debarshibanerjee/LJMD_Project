4341b0029fa6676aeab4176a199003978bccebbb
Sera91 <serafina.digioia@inaf.it>
2022-02-23 12:45:34 +0100

    constants added locally

--- src/verlet.c
+++ src/verlet.c
@@ -1,6 +1,8 @@
 #include "prototypes.h"
 #include "variables.h"
 
+const double kboltz = 0.0019872067;             /* boltzman constant in kcal/mol/K */
+const double mvsq2e = 2390.05736153349; /* m*v^2 in kcal/mol */
 
 /* velocity verlet propagation step*/
 void verlet_vel_propagation(mdsys_t* sys) {
