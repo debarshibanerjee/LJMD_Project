65b00bad8c684ba129ed0ff0e2db2c1e35b4cb2b
Debarshi Banerjee <45908114+debarshibanerjee@users.noreply.github.com>
2022-03-01 19:10:39 +0100

    started modifying force function

--- compile.sh
+++ compile.sh
@@ -6,11 +6,9 @@ rm -rf build/
 
 echo "Preparing to compile with CMake..."
 mkdir -p build
-cmake -S. -B ./build -D ENABLE_TESTING=yes
+cmake -S. -B ./build -D ENABLE_TESTING=yes -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build build/
 
 ./test_verification.sh
 
-./example_verification.sh
-
-
+# ./example_verification.sh
