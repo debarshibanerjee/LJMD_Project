690a9c34543e6551208ae66588e451c733d9de6b
Sera91 <serafina.digioia@inaf.it>
2022-03-01 11:48:44 +0100

    modified utilities.c to add functions which allocate and free arrays

--- src/utilities.c
+++ src/utilities.c
@@ -35,3 +35,64 @@ double pbc(double x, const double boxby2) {
 		x += 2.0 * boxby2;
 	return x;
 }
+
+
+void allocate_sys_arrays ( mdsys_t * const sys ) {
+
+  // allocate coordinates arrays
+  sys->rx = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->ry = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->rz = (double *) malloc( sys->natoms * sizeof(double) );
+
+  // allocate velocity arrays
+  sys->vx = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->vy = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->vz = (double *) malloc( sys->natoms * sizeof(double) );
+
+  // allocate forces arrays
+  sys->fx = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->fy = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->fz = (double *) malloc( sys->natoms * sizeof(double) );
+
+#ifdef USE_MPI
+  // allocate support array fo centers
+  sys->cx = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->cy = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->cz = (double *) malloc( sys->natoms * sizeof(double) );
+#endif //USE_MPI
+
+  // allocate forces arrays
+  sys->fx = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->fy = (double *) malloc( sys->natoms * sizeof(double) );
+  sys->fz = (double *) malloc( sys->natoms * sizeof(double) );
+  
+
+}
+
+void free_sys_arrays ( mdsys_t * const sys ) {
+
+  // free coordinates
+  free( sys->rx );
+  free( sys->ry );
+  free( sys->rz );
+  
+  // free velocities
+  free( sys->vx );
+  free( sys->vy );
+  free( sys->vz );
+  
+  
+  // free forces
+  free( sys->fx );
+  free( sys->fy );
+  free( sys->fz );
+
+#ifdef USE_MPI
+  // free support
+  free( sys->cx );
+  free( sys->cy );
+  free( sys->cz );
+#endif //USE_MPI
+  
+
+}
