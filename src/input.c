68334c4fb34879acab2137a89dec0960e79aa2be
Sera91 <serafina.digioia@inaf.it>
2022-03-01 11:49:57 +0100

    modified:   input.c

--- src/input.c
+++ src/input.c
@@ -34,7 +34,7 @@ int get_a_line(FILE* fp, char* buf) {
 	return 0;
 }
 
-int populate_data(FILE * fp, char* line, char* restfile, char* trajfile, char* ergfile, mdsys_t * sys, int * nprint)
+int populate_data(FILE * fp, char (*line)[BLEN], char (*restfile)[BLEN], char (*trajfile)[BLEN], char (*ergfile)[BLEN], mdsys_t * sys, int * nprint)
 {
 
   //int rem, nloc;
@@ -43,61 +43,61 @@ int populate_data(FILE * fp, char* line, char* restfile, char* trajfile, char* e
   //if ( sys->rank == 0 ) {
   
     // reads atom number from fp and saves it in data_struct sys
-    if (get_a_line(fp, line))
+    if (get_a_line(fp, *line))
       return 1;
-    sys.natoms = atoi(line);
+    sys->natoms = atoi(*line);
   
     // reads mass from fp and saves it in data_struct sys  
-    if (get_a_line(fp, line))
+    if (get_a_line(fp, *line))
       return 1;
-    sys.mass = atof(line);
+    sys->mass = atof(*line);
     
     // reads epsilon from fp and saves it in data_struct sys
-    if (get_a_line(fp, line))
+    if (get_a_line(fp, *line))
       return 1;
-    sys.epsilon = atof(line);
+    sys->epsilon = atof(*line);
     
     // reads sigma from fp and saves it in data_struct sys
-    if (get_a_line(fp, line))
+    if (get_a_line(fp, *line))
       return 1;
-    sys.sigma = atof(line);
+    sys->sigma = atof(*line);
     
     // reads cut-off radius from fp and saves it in data_struct sys
-    if (get_a_line(fp, line))
+    if (get_a_line(fp, *line))
       return 1;
-    sys.rcut = atof(line);
+    sys->rcut = atof(*line);
     
     // reads box-size from fp and saves it in data_struct sys
-    if (get_a_line(fp, line))
+    if (get_a_line(fp, *line))
       return 1;
-    sys.box = atof(line);
+    sys->box = atof(*line);
     
     // reads path/to/restart-file from fp and saves it in output array of char 'restfile'
-    if (get_a_line(fp, restfile))
+    if (get_a_line(fp, *restfile))
       return 1;
     
     // reads path/to/trajectories-file from fp and saves it in output array of char 'trajfile'
-    if (get_a_line(fp, trajfile))
+    if (get_a_line(fp, *trajfile))
       return 1;
     
     // reads path/to/energies-file from fp and saves it in output array of char 'ergfile'
-    if (get_a_line(fp, ergfile))
+    if (get_a_line(fp, *ergfile))
       return 1;
     
     // reads number of steps from fp and saves it in data_struct sys
-    if (get_a_line(fp, line))
+    if (get_a_line(fp, *line))
       return 1;
-    sys->nsteps = atoi(line);
+    sys->nsteps = atoi(*line);
     
     // reads size of time step from fp and saves it in data_struct sys
-    if (get_a_line(fp, line))
+    if (get_a_line(fp, *line))
       return 1;
-    sys->dt = atof(line);
+    sys->dt = atof(*line);
     
     // reads box-size from fp and saves it in data_struct sys
-    if (get_a_line(fp, line))
+    if (get_a_line(fp, *line))
       return 1;
-    nprint=atoi(line);
+    nprint=atoi(*line);
     
   //} //endif ( sys->rank == 0 )
 
