652ba701438392254f0e9a003b9a08b9c4fb1426
Alexander Trujillo <atrujilloochoa@gmail.com>
2022-03-04 17:56:13 +0100

    realistic scenario for a student laptop

--- compile.sh
+++ compile.sh
@@ -12,12 +12,14 @@ rm -rf build/
 
 echo "Preparing to compile with CMake..."
 mkdir -p build
-cmake -S. -B ./build -D ENABLE_TESTING=yes -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DENABLE_OPENMP=$1
+cmake -S. -B ./build -D ENABLE_TESTING=no -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DENABLE_OPENMP=$1
 cmake --build build/
 
 export OMP_DYNAMIC=FALSE
+export OMP_NUM_THREADS=2
 #./test_verification.sh
 
 export OMP_DYNAMIC=FALSE
+export OMP_NUM_THREADS=2
 ./result_verification.sh
 
