6e3f92f5f2039e06697e5e9e6afdca4280001970
Debarshi Banerjee <45908114+debarshibanerjee@users.noreply.github.com>
2022-03-06 23:54:19 +0100

    changed a parameter

--- Morse_Potential/plot_morse_lj.py
+++ Morse_Potential/plot_morse_lj.py
@@ -7,7 +7,7 @@ eps = 0.2379
 D = 0.2379
 sigma = 3.405
 re = pow(2,1/6)*sigma
-a = 1.07
+a = 1.05
 
 r = np.arange(3,10,0.00001)
 lj = 4 * eps * ((np.power(sigma, 12) / np.power(r, 12)) - (np.power(sigma, 6) / np.power(r, 6)))
@@ -17,7 +17,7 @@ plt.title("Plotting Morse and LJ Potential")
 plt.ylim(-2*eps, +5*eps)
 plt.ylabel('Energy')
 plt.xlabel('Distance')
-plt.text(8,0.7, 'a = 1.07', fontsize=11, bbox=dict(facecolor='red', alpha=0.3))
+plt.text(8,0.7, 'a = 1.05', fontsize=11, bbox=dict(facecolor='red', alpha=0.3))
 plt.plot(r,morse,'b', label='Morse Potential')
 plt.plot(r,lj,'g', label='Lennard-Jones Potential')
 plt.grid()
